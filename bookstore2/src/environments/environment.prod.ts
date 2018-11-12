214df41f2c5e80c67782281680c3052667241557
StepanBor <38396471+StepanBor@users.noreply.github.com>
2018-11-12 06:36:40 +0200

    f
    f

--- bookstore2/src/environments/environment.prod.ts
+++ bookstore2/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
 
-  serverURL:  'https://book-s.herokuapp.com/'
+  // serverURL:  'https://book-st.herokuapp.com/'
+  // serverURL:  'http://localhost:8080/'
+  serverURL:  'http://68.183.69.159/'
 };
